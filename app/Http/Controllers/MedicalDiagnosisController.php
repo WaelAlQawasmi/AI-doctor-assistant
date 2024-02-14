fe96e1cb5189875e44eeb3dde833f37e24fc28ef
waelalq <wael.abuomr@gmail.com>
2024-02-14 06:59:14 +0200

    testing

--- app/Http/Controllers/MedicalDiagnosisController.php
+++ app/Http/Controllers/MedicalDiagnosisController.php
@@ -14,7 +14,7 @@ class MedicalDiagnosisController extends Controller
 
     public function getBasicAIDiagnosis(Request $request)
     {
-        $this->authorize('BasicAIDiagnosis', User::class);
+      //  $this->authorize('BasicAIDiagnosis', User::class);
 
         //  $prompt = "   
         //   كمساعد في تشخيص الامراض الطبية لطبيب مختص  وكان قد جاءت حالة مرضية 
@@ -63,7 +63,7 @@ class MedicalDiagnosisController extends Controller
             يُفضل أن تتضمن أسماء الأدوية أسماء تجارية باللغة الإنجليزية وكتابتها بالغغة الانجليزية مع الجرعات اللازمة.
             يُرجى التأكد من تقديم الرد بالتنسيق المطلوب و توضيح كل نقطة بشكل دقيق ووافٍ.
         ";
-          return "500";
+          return $prompt;
         $client = OpenAI::client( getenv('OPEN_AI_TOKEN'));
         $result = $client->chat()->create([
                     'model' => 'gpt-3.5-turbo',
--- routes/api.php
+++ routes/api.php
@@ -26,16 +26,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/auth/signup', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
-Route::get('/auth/get-all-users', [AuthController::class, 'getAllusers'])->middleware('auth:sanctum|CookieToHeaderToken');
+Route::get('/auth/get-all-users', [AuthController::class, 'getAllusers'])->middleware('auth:sanctum');
 Route::get('/auth/getUsrById/{id}', [AuthController::class, 'getUsrById'])->middleware('auth:sanctum');
 Route::get('/auth/getUsrByname/{name}', [AuthController::class, 'getUsrByName'])->middleware('auth:sanctum');
-Route::middleware([CookieToHeaderToken::class, 'auth:sanctum'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/auth/activate', [AuthController::class, 'activateUser']);
     Route::post('/auth/deactivate', [AuthController::class, 'deactivateUser']);
     Route::get('/point-management/users-with-points', [PointManagementController::class, 'getAllusersWithPoints'])->middleware(['can:manage users']);
     Route::post('/payments/granting-trial-period', [PaymentsController::class, 'grantingTrialPeriod'])->middleware(['can:manage users']);
     Route::post('/payments/add-points-to-user', [PaymentsController::class, 'addPointsToUser'])->middleware(['can:manage users']);
-    Route::get('/main-page/userProfile', [AuthController::class, 'getUserProfile'])->middleware(['CookieToHeaderToken',CookieToHeaderToken::class]);
+    Route::get('/main-page/userProfile', [AuthController::class, 'getUserProfile']);
     Route::post('/medical-diagnosis',[MedicalDiagnosisController::class,'getBasicAIDiagnosis'])->middleware(['CookieToHeaderToken',CookieToHeaderToken::class]);
 
 });

ede93e66d271cd98e146230abc1f47433b406b50
waelalq <wael.abuomr@gmail.com>
2024-02-23 03:07:57 +0200

    stable v2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import './bootstrap';
 import { createApp } from 'vue'
 import router from "./vueApp/router/index.js";
+import 'tailwindcss/tailwind.css';
+
+
 
 import  App from './vueApp/views/App.vue'
 const app = createApp(App).use(router)
--- resources/js/vueApp/router/requestActions.js
+++ resources/js/vueApp/router/requestActions.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { logOut } from '@/Mixins/authMixin.js';
 
-//const apiUrl =  'http://127.0.0.1:8000/api';
-const apiUrl = 'https://www.ai.black-ch.com/api'; 
+const apiUrl =  'http://127.0.0.1:8000/api';
+//const apiUrl = 'https://www.ai.black-ch.com/api'; 
 
 export const fetchData = async (endpoint) => {
   try {

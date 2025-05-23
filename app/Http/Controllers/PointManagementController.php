941b587ea9838aa8f89606c02328ea6512ea1a88
wael k q <wael.abuomr@gmail.com>
2025-05-23 23:05:08 +0300

    remove useless code

--- app/Http/Controllers/PointManagementController.php
+++ app/Http/Controllers/PointManagementController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\payments;
 use App\Models\pointManagement;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class PointManagementController extends Controller
